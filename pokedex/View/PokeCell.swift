d06cf515596ed7e17a975342f5cebf6ae4ba6b05
Binh Nguyen <nnabinh@gmail.com>
2017-09-03 21:11:51 +0900

    Add pokecell & connect collectionview delegate/datasource with storyboard

---
+++ pokedex/View/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Nguyen Ngo An Binh on 2017/09/03.
+//  Copyright © 2017 Nguyen Ngo An Binh. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
